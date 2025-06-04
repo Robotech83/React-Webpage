9b79e2a0ffbfdac87262b564aec5582629a8b929
Robotech83 <volkswagenjetta@protonmail.com>
2025-06-04 15:14:59 -0700

    update linter

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import' // Add this line
 
 export default [
   { ignores: ['dist'] },
@@ -19,10 +20,12 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin, // Add this line
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules, // Add this line
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { ThemeUIProvider } from 'theme-ui'; 
 import { theme } from './theme/theme';
 
-import { TopBar } from './components/TopAndNavBar/TopBar';
-import { NavBar } from './components/TopAndNavBar/NavBar';
+// import { TopBar } from './components/TopAndNavBar/TopBar';
+// import { NavBar } from './components/TopAndNavBar/NavBar';
+// import Header from './components/Header/Header';
+// import Robotics from './Pages/Robotics';
+// import Footer from './components/Footer/Footer';
+// import Subfooter from './components/Footer/Subfooter';
+
+import TopBar from './components/TopAndNavBar/TopBar';
+import NavBar from './components/TopAndNavBar/NavBar'; 
 import Header from './components/Header/Header';
 import Robotics from './Pages/Robotics';
 import Footer from './components/Footer/Footer';
 import Subfooter from './components/Footer/Subfooter';
-
 
 
 function App() {
