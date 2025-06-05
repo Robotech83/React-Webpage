7d207e7acd4ee436f3e84225dff89a59ea5fec8c
Robotech83 <volkswagenjetta@protonmail.com>
2025-06-05 06:59:54 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
 import React from 'react';
-import { ThemeUIProvider } from 'theme-ui';
-import { theme } from './theme/theme'; // ✅ Import your theme object
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'theme-ui';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
-import { TopBar } from './components/TopAndNavBar/TopBar'; //name import 
-import { NavBar } from './components/TopAndNavBar/NavBar';
-
-import Header from './components/Header/Header';
+import { theme } from './theme/theme';
+import { WebDev } from './Pages/';
 import Robotics from './Pages/Robotics';
+import About from './Pages/About'; // ✅ Make sure this path matches your folder structure
+
+import { TopBar } from './components/TopAndNavBar/TopBar';
+import { NavBar } from './components/TopAndNavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Subfooter from './components/Footer/Subfooter';
 
-//import './assets/css/projects/topBar.css';
-//import './assets/css/projects/navBar.css'
-
-
-
-function App() {
-  return (
-    <>
- <ThemeUIProvider theme={theme}> 
-    <div>
-       <TopBar/>
-        <NavBar/>
-        <Header />
-        <Robotics />
-
-        <div className="footer">
-          <Footer />
-          <Subfooter />
-        </div>
-    </div>
- </ThemeUIProvider>
-
-    </>
-  );
-}
-
-export default App;
-
-
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ThemeProvider theme={theme}>
+    <Router>
+      <TopBar />
+      <NavBar />
+      <Routes>
+        <Route path="/web-dev" element={<WebDev />} />
+        <Route path="/robotics" element={<Robotics />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+      <Subfooter />
+    </Router>
+  </ThemeProvider>
+);
