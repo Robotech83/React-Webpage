c86b260c31264dd1361b4a65814460e4ae51b19b
Robotech83 <volkswagenjetta@protonmail.com>
2025-06-04 22:19:12 -0700

    update

--- src/components/TopAndNavBar/NavBar.jsx
+++ src/components/TopAndNavBar/NavBar.jsx
@@ -1,20 +1,57 @@
+/** @jsxImportSource theme-ui */
 import React from "react";
+import { Box, Link, NavLink } from "theme-ui";
 
-//import './navBar.css';
+export const NavBar = () => {
+  return (
+    <Box as="nav" sx={{ display: "flex", gap: 4, px: 4, py: 3, bg: "background", alignItems: "center" }}>
+      {/* Home */}
+      <NavLink href="/" sx={{ variant: "styles.navLink" }}>Home</NavLink>
 
-// This component defines a navigation bar with links to different sections of the portfolio website.
- export const NavBar = () => {
-    return (
-        <div className="navbar">
-        <div className="navbar-container">
-            <div className="navbar-left">
-            <a href="#home" className="navbar-link">Home</a>
-            <a href="#about" className="navbar-link">About</a>
-            <a href="#projects" className="navbar-link">Projects</a>
-            <a href="#contact" className="navbar-link">Contact</a>
-            </div>
-        </div>
-        </div>
-    );
-    }
+      {/* About with dropdown */}
+      <Box sx={{ position: "relative", ":hover .dropdown": { display: "block" } }}>
+        <NavLink href="#" sx={{ variant: "styles.navLink" }}>About</NavLink>
+        <Box className="dropdown" sx={dropdownMenuStyles}>
+          <NavLink href="/about" sx={dropdownItem}>Bio</NavLink>
+          <NavLink href="/resume" sx={dropdownItem}>Resume</NavLink>
+        </Box>
+      </Box>
 
+      {/* Portfolio with dropdown */}
+      <Box sx={{ position: "relative", ":hover .dropdown": { display: "block" } }}>
+        <NavLink href="#" sx={{ variant: "styles.navLink" }}>Portfolio</NavLink>
+        <Box className="dropdown" sx={dropdownMenuStyles}>
+          <NavLink href="/web-dev" sx={dropdownItem}>Web Dev</NavLink>
+          <NavLink href="/robotics" sx={dropdownItem}>Robotics</NavLink>
+          <NavLink href="/gamedev" sx={dropdownItem}>Game Dev</NavLink>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+// Reusable dropdown styles
+const dropdownMenuStyles = {
+  display: "none",
+  position: "absolute",
+  top: "100%",
+  left: 0,
+  bg: "background",
+  boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+  p: 2,
+  borderRadius: 2,
+  zIndex: 99,
+  minWidth: "160px",
+};
+
+const dropdownItem = {
+  display: "block",
+  px: 3,
+  py: 2,
+  textDecoration: "none",
+  color: "text",
+  ":hover": {
+    bg: "primary",
+    color: "background"
+  }
+};
